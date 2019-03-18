d5db7c2fc09588e12640d4f623545b4dda3b3e0d
James Rupa <jamesjrupa@gmail.com>
2019-03-17 21:45:21 -0400

    RESOURCES

--- src/com/jamesrupa/pokemon/Launcher/Pokemon.java
+++ src/com/jamesrupa/pokemon/Launcher/Pokemon.java
@@ -1,10 +1,13 @@
 package com.jamesrupa.pokemon.Launcher;
 
+import com.jamesrupa.pokemon.Sound.Sound;
+
 public class Pokemon {
     public static void main(String[] args) {
         // MAIN CLASS
         // FIND DIMENSIONS TO DISPLAY
         Game game = new Game("Pokémon", 1400, 746);
         game.start();
+        Sound.startscreen.loop();
     }
 }
